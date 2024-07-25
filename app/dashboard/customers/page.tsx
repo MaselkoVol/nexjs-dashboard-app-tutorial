b280ff8a7791cca881ea07a316f09d6ef79e3c99
Володимир Маселко <kartoshka8800@gmail.com>
2024-07-25 19:55:42 +0300

    created app structure and started making dashboard route

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+	return (
+		<p>Customers Page</p>
+	)
+}
+
+export default page	
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+	return (
+		<p>Invoices page</p>
+	)
+}
+
+export default page
